ee5324a316878b483d0c83f8f784e037fd72903e
Geider10 <geiderde20@gmail.com>
2024-03-07 00:39:54 -0300

    agregue una validacion para el input y el tipo de titulo que se agrega

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
 const d = document;
-const txtArea = d.getElementById("area");
-txtArea.addEventListener("keyup",e=>{
-    txtArea.style.height = "54px";
-    let scHeight = e.target.scrollHeight;
-    txtArea.style.height = `${scHeight}px`;
+// const txtArea = d.getElementById("area");
+// txtArea.addEventListener("keyup",e=>{
+//     txtArea.style.height = "54px";
+//     let scHeight = e.target.scrollHeight;
+//     txtArea.style.height = `${scHeight}px`;
+// })
+
+const btnAgregar = d.getElementById("btnAgregar");
+const iptTitulo = d.getElementById("iptTitulo");
+const typeTitulo = d.getElementById("typeTitulo");
+btnAgregar.addEventListener("click",()=>{
+    if(checkInput().length >1 && checkTitulos() == "Tarea"){
+        console.log("Agrego una tarea")
+    }
+    else if( checkInput().length >1 && checkTitulos() == "Lista"){
+       console.log("Agrego una lista");
+    }
+    else{
+        console.log("No agrego nada");
+    }
 })
+const checkInput = ()=>{
+    const convert = iptTitulo.value.toLowerCase();
+    const firstChar = convert.slice(0,1);
+    return convert.replace(firstChar,firstChar.toUpperCase());
+}
+const checkTitulos = ()=>{
+    typeTitulo.addEventListener("change",(e)=>{
+        return e.target.value;
+    })
+    return typeTitulo.value;
+}
