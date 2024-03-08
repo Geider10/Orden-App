f45db78db2db4060ae5f19a25bdcde85c8431455
Geider10 <geiderde20@gmail.com>
2024-03-07 23:41:00 -0300

    agregue la logica para crear tareas y listas

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
 const d = document;
-// const txtArea = d.getElementById("area");
-// txtArea.addEventListener("keyup",e=>{
-//     txtArea.style.height = "54px";
-//     let scHeight = e.target.scrollHeight;
-//     txtArea.style.height = `${scHeight}px`;
-// })
-
 const btnAgregar = d.getElementById("btnAgregar");
 const iptTitulo = d.getElementById("iptTitulo");
 const typeTitulo = d.getElementById("typeTitulo");
+const mainLista = d.getElementById("mainLista");
+const bodyTablero = d.getElementById("bodyTablero");
 btnAgregar.addEventListener("click",()=>{
     if(checkInput().length >1 && checkTitulos() == "Tarea"){
-        console.log("Agrego una tarea")
+        const taskLista = d.createElement("div");
+        taskLista.classList.add("lista__tarea")
+        taskLista.innerHTML = //html
+        `
+        <div class="tarea__icons"> 
+           <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg></span>
+           <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-vertical" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></span>
+        </div>
+        <textarea placeholder="Nombre de la tarea" required class="area">${checkInput()}</textarea>
+        `
+        mainLista.appendChild(taskLista);
+        autoSizeTxt();
+        iptTitulo.value=" ";
+
     }
     else if( checkInput().length >1 && checkTitulos() == "Lista"){
-       console.log("Agrego una lista");
+        const tableroLista = d.createElement("div");
+        tableroLista.classList.add("tablero__lista");
+        tableroLista.innerHTML=//html
+        `
+        <div class="lista__nombre">
+            <input type="text" value="${checkInput()}"readonly="true">
+        </div>
+        `
+        bodyTablero.appendChild(tableroLista);
+        iptTitulo.value=" ";
     }
     else{
         console.log("No agrego nada");
@@ -31,3 +48,19 @@ const checkTitulos = ()=>{
     })
     return typeTitulo.value;
 }
+const autoSizeTxt =()=>{
+    const txtArea = d.querySelectorAll(".area");
+    txtArea.forEach(txt =>{
+        txt.addEventListener("keyup",e=>{
+            txt.style.height = "54px";
+            let scHeight = e.target.scrollHeight;
+            txt.style.height = `${scHeight}px`;
+        })
+    })
+
+}
+// txtArea.addEventListener("keyup",e=>{
+//     txtArea.style.height = "54px";
+//     let scHeight = e.target.scrollHeight;
+//     txtArea.style.height = `${scHeight}px`;
+// })
