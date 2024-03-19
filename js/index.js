9dc9936930f28b354fa7bdc7ea774f2678f696bc
Geider10 <geiderde20@gmail.com>
2024-03-18 22:52:43 -0300

    Agregue logica para editar el nombre de la tarea y lista

--- js/index.js
+++ js/index.js
@@ -2,43 +2,47 @@ const d = document;
 const btnAgregar = d.getElementById("btnAgregar");
 const iptTitulo = d.getElementById("iptTitulo");
 const typeTitulo = d.getElementById("typeTitulo");
-const mainLista = d.getElementById("mainLista");
 const bodyTablero = d.getElementById("bodyTablero");
+
 btnAgregar.addEventListener("click",()=>{
-    if(checkInput().length >1 && checkTitulos() == "Tarea"){
+    if(checkInput(iptTitulo).length >1 && checkTitulos() == "Tarea"){
         const taskLista = d.createElement("div");
         taskLista.classList.add("lista__tarea")
         taskLista.innerHTML = //html
         `
         <div class="tarea__icons"> 
-           <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg></span>
-           <span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-vertical" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></span>
+            <span class="off"><svg  xmlns="http://www.w3.org/2000/svg"width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg></span>
+            <span><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg></span>
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg></span>
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></span>
         </div>
-        <textarea placeholder="Nombre de la tarea" required class="area">${checkInput()}</textarea>
+        <textarea placeholder="Nombre de la tarea" readonly >${checkInput(iptTitulo)}</textarea>
         `
-        mainLista.appendChild(taskLista);
-        autoSizeTxt();
-        iptTitulo.value=" ";
-
+        autoScale(taskLista.children[1]);
+        editNameTxt(taskLista.children[1],taskLista.children[0].children[1],taskLista.children[0].children[0]);
+        bodyTablero.children[0].appendChild(taskLista);
     }
-    else if( checkInput().length >1 && checkTitulos() == "Lista"){
+    else if(checkInput(iptTitulo).length >1 && checkTitulos() == "Lista"){
         const tableroLista = d.createElement("div");
         tableroLista.classList.add("tablero__lista");
         tableroLista.innerHTML=//html
         `
         <div class="lista__nombre">
-            <input type="text" value="${checkInput()}"readonly="true">
+            <input type="text" value="${checkInput(iptTitulo)}"readonly>
+            <div>
+                <span><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg></span>
+                <span class="off"><svg  xmlns="http://www.w3.org/2000/svg"width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg></span>
+            </div>
         </div>
         `
+        editNameTxt(tableroLista.children[0].children[0],tableroLista.children[0].children[1].children[0],tableroLista.children[0].children[1].children[1]);
         bodyTablero.appendChild(tableroLista);
-        iptTitulo.value=" ";
+        enabledTask();
     }
-    else{
-        console.log("No agrego nada");
-    }
+    iptTitulo.value="";
 })
-const checkInput = ()=>{
-    const convert = iptTitulo.value.toLowerCase();
+const checkInput = (txt)=>{
+    const convert = txt.value.toLowerCase();
     const firstChar = convert.slice(0,1);
     return convert.replace(firstChar,firstChar.toUpperCase());
 }
@@ -48,19 +52,35 @@ const checkTitulos = ()=>{
     })
     return typeTitulo.value;
 }
-const autoSizeTxt =()=>{
-    const txtArea = d.querySelectorAll(".area");
-    txtArea.forEach(txt =>{
-        txt.addEventListener("keyup",e=>{
-            txt.style.height = "54px";
-            let scHeight = e.target.scrollHeight;
-            txt.style.height = `${scHeight}px`;
-        })
+const autoScale = (area)=>{
+    area.addEventListener("keyup",e=>{
+        area.style.height = "54px";
+        let sHeight = e.target.scrollHeight;
+        area.style.height = `${sHeight}px`;
     })
-
 }
-// txtArea.addEventListener("keyup",e=>{
-//     txtArea.style.height = "54px";
-//     let scHeight = e.target.scrollHeight;
-//     txtArea.style.height = `${scHeight}px`;
-// })
+const editNameTxt = (area,btnEdit,btnCheck)=>{
+    let edit = false;
+    area.addEventListener("mousedown",(e)=>{
+       if(edit == false){
+        e.target.style.border = "none"
+       }
+    });
+    btnEdit.addEventListener("click",()=>{
+        area.style.border = "1px solid black";
+        area.removeAttribute("readonly");
+        btnCheck.classList.remove("off");
+        btnEdit.classList.add("off");
+        edit = true;
+    })
+    btnCheck.addEventListener("click",()=>{
+        area.style.border = "none";
+        area.setAttribute("readonly","true");
+        btnCheck.classList.add("off");
+        btnEdit.classList.remove("off");
+        area.value = checkInput(area);
+    })
+}
+const enabledTask = ()=>{
+    typeTitulo.children[1].removeAttribute("disabled");
+}
