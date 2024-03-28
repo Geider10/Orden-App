df450a18c63fd6c761c41a8e0ff45f5beda4e62f
Geider10 <geiderde20@gmail.com>
2024-03-28 00:09:15 -0300

    agregue estilo en fondo y mejore la accesibilidad

--- js/index.js
+++ js/index.js
@@ -1,84 +1,100 @@
 document.addEventListener("DOMContentLoaded", () => {
 const d = document;
+const btnMode = d.getElementById("btnMode");
 const btnAgregar = d.getElementById("btnAgregar");
 const iptTitulo = d.getElementById("iptTitulo");
 const typeTitulo = d.getElementById("typeTitulo");
+const tituloError = d.getElementById("tituloError");
 const bodyTablero = d.getElementById("bodyTablero");
 
 btnAgregar.addEventListener("click",()=>{
-    if(checkInput(iptTitulo).length >1 && checkTitulos() == "Tarea"){
-        const taskLista = d.createElement("div");
-        taskLista.classList.add("lista__tarea");
-        taskLista.classList.add("ancla");
-        taskLista.setAttribute("data-taskLight","true");
-        taskLista.innerHTML = //html
-        `
-        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-        <details>
-            <summary><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></summary>
-            <ul>
-               <li >Editar</li>
-               <li class="off">Guardar</li>
-               <li >Eliminar</li>
-           </ul>
-        </details>
-        <textarea placeholder="Nombre de la tarea" readonly data-colorLight>${checkInput(iptTitulo)}</textarea>
-        `
-        const iptTt = taskLista.children[2];
-        const btnEditt = taskLista.children[1].children[1].children[0];
-        const btnGt = taskLista.children[1].children[1].children[1];
-        const btnDeletet =  taskLista.children[1].children[1].children[2];
-        const bntStar = taskLista.children[0]
-        autoScale(iptTt);
-        editNameTxt(iptTt,btnEditt,btnGt,btnDeletet,taskLista);
-        destacarTask(bntStar,"fill");
-        if(localStorage.getItem("tema") == "light"){
-            taskLista.classList.add("taskLight");
-            iptTt.classList.add("colorLight");
-        }
-        bodyTablero.children[0].appendChild(taskLista);
-       
-    }
-    else if(checkInput(iptTitulo).length >1 && checkTitulos() == "Lista"){
-        const tableroLista = d.createElement("div");
-        tableroLista.classList.add("tablero__lista");
-        tableroLista.classList.add("listas");
-        tableroLista.setAttribute("data-listLight","true");
-        tableroLista.innerHTML=//html
-        `
-        <div class="lista__nombre nombresLista" >
-            <input  type="text" value="${checkInput(iptTitulo)}"readonly data-colorLight>
-            <details>
-               <summary><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></summary>
-                <ul>
-                 <li >Editar</li>
-                 <li class="off">Guardar</li>
-                 <li >Eliminar</li>
-                </ul>
-           </details>
-        </div>
-        `
-        const iptT =tableroLista.children[0].children[0];
-        const btnEdit =tableroLista.children[0].children[1].children[1].children[0];
-        const btnG = tableroLista.children[0].children[1].children[1].children[1];
-        const btnDelete = tableroLista.children[0].children[1].children[1].children[2];
-
-        editNameTxt(iptT,btnEdit,btnG,btnDelete,tableroLista);
-        typeTitulo.children[1].removeAttribute("disabled");
-        if(localStorage.getItem("tema") == "light"){
-            tableroLista.classList.add("listLight");
-            iptT.classList.add("colorLight");
-        }
-        bodyTablero.appendChild(tableroLista);
-        addSortListas();
-        addSortTablero();
-    }
+    checkTitulos() == "Lista"? createList():createTask();
     iptTitulo.value="";
+    btnAgregar.classList.add("off");
+    tituloError.classList.remove("off");
 })
+const createTask=()=>{
+    const taskLista = d.createElement("div");
+    taskLista.classList.add("lista__tarea");
+    taskLista.classList.add("ancla");
+    taskLista.setAttribute("data-taskLight","true");
+    taskLista.innerHTML = //html
+    `
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
+    <details>
+        <summary><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></summary>
+        <ul>
+           <li >Editar</li>
+           <li class="off">Guardar</li>
+           <li >Eliminar</li>
+       </ul>
+    </details>
+    <textarea placeholder="Nombre de la tarea" readonly data-colorLight>${checkInput(iptTitulo)}</textarea>
+    `
+    const iptTt = taskLista.children[2];
+    const btnEditt = taskLista.children[1].children[1].children[0];
+    const btnGt = taskLista.children[1].children[1].children[1];
+    const btnDeletet =  taskLista.children[1].children[1].children[2];
+    const bntStar = taskLista.children[0]
+    autoScale(iptTt);
+    editNameTxt(iptTt,btnEditt,btnGt,btnDeletet,taskLista);
+    destacarTask(bntStar,"fill");
+    if(localStorage.getItem("tema") == "light"){
+        taskLista.classList.add("taskLight");
+        iptTt.classList.add("colorLight");
+    }
+    bodyTablero.children[0].appendChild(taskLista);
+   
+}
+const createList=()=>{
+    const tableroLista = d.createElement("div");
+    tableroLista.classList.add("tablero__lista");
+    tableroLista.classList.add("listas");
+    tableroLista.setAttribute("data-listLight","true");
+    tableroLista.innerHTML=//html
+    `
+    <div class="lista__nombre nombresLista" >
+        <input  type="text" value="${checkInput(iptTitulo)}"readonly data-colorLight>
+        <details>
+           <summary><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg></summary>
+            <ul>
+             <li >Editar</li>
+             <li class="off">Guardar</li>
+             <li >Eliminar</li>
+            </ul>
+       </details>
+    </div>
+    `
+    const iptT =tableroLista.children[0].children[0];
+    const btnEdit =tableroLista.children[0].children[1].children[1].children[0];
+    const btnG = tableroLista.children[0].children[1].children[1].children[1];
+    const btnDelete = tableroLista.children[0].children[1].children[1].children[2];
+
+    editNameTxt(iptT,btnEdit,btnG,btnDelete,tableroLista);
+    typeTitulo.children[1].removeAttribute("disabled");
+    if(localStorage.getItem("tema") == "light"){
+        tableroLista.classList.add("listLight");
+        iptT.classList.add("colorLight");
+    }
+    bodyTablero.appendChild(tableroLista);
+    addSortListas();
+}
 const checkInput = (txt)=>{
     const convert = txt.value.toLowerCase();
     const firstChar = convert.slice(0,1);
     return convert.replace(firstChar,firstChar.toUpperCase());
+}
+const inputError = ()=>{
+    iptTitulo.addEventListener("input",()=>{
+        if(checkInput(iptTitulo).length>=3){
+            btnAgregar.classList.remove("off");
+            tituloError.classList.add("off");
+        }
+        else{
+            btnAgregar.classList.add("off");
+            tituloError.classList.remove("off");
+        }
+    })
 }
 const checkTitulos = ()=>{
     typeTitulo.addEventListener("change",(e)=>{
@@ -144,11 +160,8 @@ const addSortListas =()=>{
     listasTablero.forEach(l=>{
         new Sortable(l,{group:"shared",handle:".ancla",animation:150});
     })
+    new Sortable(bodyTablero,{sort:"true",handle:".tablero__lista"});
 }
-const addSortTablero =()=>{
-    new Sortable(bodyTablero,{sort:"true"});
-}
-const btnMode = d.getElementById("btnMode");
 btnMode.addEventListener("click",(e)=>{
     e.preventDefault();
     btnMode.textContent == "☀️"? changeTema("light"): changeTema("dark");
@@ -178,6 +191,5 @@ const changeTema = (tipoTema)=>{
 localStorage.getItem("tema") == null && localStorage.setItem("tema","dark");
 localStorage.getItem("tema") == "dark" && changeTema("dark");
 localStorage.getItem("tema") == "light" && changeTema("light");
-
-console.log("Hola todo bien");
+inputError();
 });
